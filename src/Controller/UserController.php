eae16e751a622da00724827dc2df9c544be30a47
slimayadi20 <slim.ayadi@esprit.tn>
2022-02-12 12:22:27 +0100

    second commit

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,11 +5,11 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use App\Form\UserFormType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-
 class UserController extends AbstractController
 {
     /**
@@ -38,11 +38,51 @@ class UserController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
+            $this->addFlash('success' , 'L"action a été effectué');
+            return $this->redirectToRoute("user");
+
         }
         return $this->render("user/ajouter.html.twig", [
             "form_title" => "Ajouter un user",
             "form_user" => $form->createView(),
         ]);
+    }
+    /**
+     * @Route("/modifyUser/{id}", name="modifyUser")
+     */
+    public function modifyUser(Request $request, int $id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $user = $entityManager->getRepository(User::class)->find($id);
+        $form = $this->createForm(UserFormType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $entityManager->flush();
+            $this->addFlash('success' , 'L"action a été effectué');
+            return $this->redirectToRoute("user");
+        }
+
+        return $this->render("user/modifier.html.twig", [
+            "form_title" => "Modifier un user",
+            "form_user" => $form->createView(),
+        ]);
+    }
+    /**
+     * @Route("/deleteUser/{id}", name="deleteUser")
+     */
+    public function deleteUser(int $id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $user = $entityManager->getRepository(User::class)->find($id);
+        $entityManager->remove($user);
+        $entityManager->flush();
+        $this->addFlash('success' , 'L"action a été effectué');
+
+
+        return $this->redirectToRoute("user");
     }
 
 
