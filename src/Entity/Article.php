3c7295c9f764e17fa32f3850f867d54ae865ee55
malakhachicha1998 <98495690+malakhachicha1998@users.noreply.github.com>
2022-02-14 12:17:55 +0100

    Article Done

---
+++ src/Entity/Article.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ */
+class Article
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=40)
+     * @Assert\NotBlank (message = "le titre est obligatoire")
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=35)
+     */
+    private $dateCreation;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message =" la description est obligatoire")
+     */
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?string
+    {
+        return $this->dateCreation;
+    }
+
+    public function setDateCreation(string $dateCreation): self
+    {
+        $this->dateCreation = $dateCreation;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
