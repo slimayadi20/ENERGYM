978a2d12737db4c8f994b2a06a4e69e8ff39a51b
fedibenmansour <fedi.benmansour@esprit.tn>
2022-02-22 19:46:56 +0100

    validation1

--- migrations/Version20220222182534.php
+++ migrations/Version20220222182534.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220221203710 extends AbstractMigration
+final class Version20220222182534 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,7 @@ final class Version20220221203710 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE reclamation ADD statut VARCHAR(255) NOT NULL, CHANGE nom_user_id nom_user_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE reclamation ADD nom_user_id INT DEFAULT NULL, ADD statut VARCHAR(255) NOT NULL');
         $this->addSql('ALTER TABLE reclamation ADD CONSTRAINT FK_CE60640452E1D85D FOREIGN KEY (nom_user_id) REFERENCES user (id)');
         $this->addSql('CREATE INDEX IDX_CE60640452E1D85D ON reclamation (nom_user_id)');
     }
@@ -30,6 +30,6 @@ final class Version20220221203710 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE reclamation DROP FOREIGN KEY FK_CE60640452E1D85D');
         $this->addSql('DROP INDEX IDX_CE60640452E1D85D ON reclamation');
-        $this->addSql('ALTER TABLE reclamation DROP statut, CHANGE nom_user_id nom_user_id INT NOT NULL');
+        $this->addSql('ALTER TABLE reclamation DROP nom_user_id, DROP statut');
     }
 }
