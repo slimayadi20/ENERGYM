e1a7949a11a7d695e9b00cc8470caaf7da356342
fedibenmansour <fedi.benmansour@esprit.tn>
2022-02-22 19:21:11 +0100

    integ front +back product+categorie

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 use App\Entity\Produit;
+use App\Entity\Categories;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,12 +16,15 @@ class ShopController extends AbstractController
     {
         $entityManager = $this->getDoctrine()->getManager();
         $produit = $entityManager->getRepository(produit::class)->findAll();
-
+        $Categories = $entityManager->getRepository(Categories::class)->findAll();
 
         return $this->render('shop/index.html.twig', [
             'controller_name' => 'ShopController',
             'listProduit' => $produit,
+            'controller_name' => 'ShopController',
+            'listCategories' => $Categories,
         ]);
     }
+
 
 }
--- src/Form/SalleFormType.php
+++ src/Form/SalleFormType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class SalleFormType extends AbstractType
 {
@@ -21,7 +22,29 @@ class SalleFormType extends AbstractType
             ->add('prix')
             ->add('heureo')
             ->add('heuref')
-            ->add('image',FileType::class)
+            ->add('image', FileType::class, [
+                'label' => 'Image (image file)',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '2048k',
+                        'mimeTypes' => [
+                            'image/*',
+
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid image',
+                    ])
+                ],
+            ])
 
 
         ;
