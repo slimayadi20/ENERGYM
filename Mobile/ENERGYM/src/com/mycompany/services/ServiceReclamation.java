b6432d06a806d57ac0ca398d94b6b7e1cd904102
slimayadi20 <slim.ayadi@esprit.tn>
2022-03-21 12:40:02 +0100

    mobile Commit

---
+++ Mobile/ENERGYM/src/com/mycompany/services/ServiceReclamation.java
@@ -0,0 +1,173 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.services;
+
+import com.codename1.io.CharArrayReader;
+import com.codename1.io.ConnectionRequest;
+import com.codename1.io.JSONParser;
+import com.codename1.io.NetworkEvent;
+import com.codename1.io.NetworkManager;
+import com.codename1.ui.Dialog;
+import com.codename1.ui.events.ActionListener;
+import com.mycompany.entities.Reclamation;
+import com.mycompany.entities.SessionManager;
+import com.mycompany.utils.Statics;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+/**
+ *
+ * @author MSI
+ */
+public class ServiceReclamation {
+    
+
+
+    public ArrayList<Reclamation> Reclamation;
+    
+    public static ServiceReclamation instance=null;
+    public boolean resultOK;
+    private ConnectionRequest req;
+
+    private ServiceReclamation() {
+         req = new ConnectionRequest();
+    }
+
+    public static ServiceReclamation getInstance() {
+        if (instance == null) {
+            instance = new ServiceReclamation();
+        }
+        return instance;
+    }
+  public ArrayList<Reclamation> parseReclamation(String jsonText){
+        try {
+            Reclamation=new ArrayList<>();
+            JSONParser j = new JSONParser();// Instanciation d'un objet JSONParser permettant le parsing du résultat json
+
+            /*
+                On doit convertir notre réponse texte en CharArray à fin de
+            permettre au JSONParser de la lire et la manipuler d'ou vient 
+            l'utilité de new CharArrayReader(json.toCharArray())
+            
+            La méthode parse json retourne une MAP<String,Object> ou String est 
+            la clé principale de notre résultat.
+            Dans notre cas la clé principale n'est pas définie cela ne veux pas
+            dire qu'elle est manquante mais plutôt gardée à la valeur par defaut
+            qui est root.
+            En fait c'est la clé de l'objet qui englobe la totalité des objets 
+                    c'est la clé définissant le tableau de tâches.
+            */
+            Map<String,Object> ReclamationListJson = j.parseJSON(new CharArrayReader(jsonText.toCharArray()));
+            
+              /* Ici on récupère l'objet contenant notre liste dans une liste 
+            d'objets json List<MAP<String,Object>> ou chaque Map est une tâche.               
+            
+            Le format Json impose que l'objet soit définit sous forme
+            de clé valeur avec la valeur elle même peut être un objet Json.
+            Pour cela on utilise la structure Map comme elle est la structure la
+            plus adéquate en Java pour stocker des couples Key/Value.
+            
+            Pour le cas d'un tableau (Json Array) contenant plusieurs objets
+            sa valeur est une liste d'objets Json, donc une liste de Map
+            */
+            List<Map<String,Object>> list = (List<Map<String,Object>>)ReclamationListJson.get("root");
+            
+            //Parcourir la liste des tâches Json
+            for(Map<String,Object> obj : list){
+                //Création des tâches et récupération de leurs données
+                Reclamation t = new Reclamation();
+                float id = Float.parseFloat(obj.get("id").toString());
+                t.setId((int)id);
+                t.setTitre(obj.get("titre").toString());
+                t.setContenu(obj.get("contenu").toString());
+                t.setStatut(obj.get("statut").toString());
+                        System.out.println("******");
+                        System.out.println(id);
+                        System.out.println("******");
+                        System.out.println("******");
+                //Ajouter la tâche extraite de la réponse Json à la liste
+                Reclamation.add(t);
+            }
+            
+            
+        } catch (IOException ex) {
+            
+        }
+         /*
+            A ce niveau on a pu récupérer une liste des tâches à partir
+        de la base de données à travers un service web
+        
+        */
+        return Reclamation;
+    }
+    public ArrayList<Reclamation> getAllReclamations(){
+        ArrayList<Reclamation> listReclamation = new ArrayList<>();
+
+        String url = Statics.BASE_URL+"/displayreclamationMobile?NomUser="+SessionManager.getId();
+        req.setUrl(url);
+        req.setPost(false);
+        req.addResponseListener(new ActionListener<NetworkEvent>() {
+            @Override
+            public void actionPerformed(NetworkEvent evt) {
+                Reclamation = parseReclamation(new String(req.getResponseData()));
+                req.removeResponseListener(this);
+            }
+        });
+        NetworkManager.getInstance().addToQueueAndWait(req);
+        return Reclamation;
+    }
+ public boolean addReclamation(Reclamation t) {
+        String url = Statics.BASE_URL+"/addReclamationMobile?titre=" + t.getTitre()+ "&contenu="+t.getContenu()+"&NomUser="+SessionManager.getId(); //création de l'URL
+        req.setUrl(url);// Insertion de l'URL de notre demande de connexion
+System.out.println(url);
+        req.addResponseListener(new ActionListener<NetworkEvent>() {
+            @Override
+            public void actionPerformed(NetworkEvent evt) {
+                resultOK = req.getResponseCode() == 200; //Code HTTP 200 OK
+                req.removeResponseListener(this); //Supprimer cet actionListener
+                /* une fois que nous avons terminé de l'utiliser.
+                La ConnectionRequest req est unique pour tous les appels de 
+                n'importe quelle méthode du Service task, donc si on ne supprime
+                pas l'ActionListener il sera enregistré et donc éxécuté même si 
+                la réponse reçue correspond à une autre URL(get par exemple)*/
+                
+            }
+        });
+        NetworkManager.getInstance().addToQueueAndWait(req);
+        return resultOK;
+    }
+     public void deletReclamation(float id){   
+        
+        Dialog d = new Dialog();
+            if(d.show("Delete reclamation","Do you really want to remove this reclamation","Yes","No"))
+            {             
+                
+                req.setUrl(Statics.BASE_URL+"/deleteReclamationMobile?id="+id);
+                //System.out.println(Statics.BASE_URL+"/deleteReclamationMobile?id="+id);
+                NetworkManager.getInstance().addToQueueAndWait(req);
+                
+                d.dispose();
+            }
+    }
+ 
+
+    public boolean updateReclamation(Reclamation t) {
+ String url = Statics.BASE_URL + "/updateReclamationMobile?id="+t.getId() +"&titre="+ t.getTitre()+ "&contenu=" + t.getContenu();
+        System.out.println(url);
+        System.out.println(t.getId());
+        req.setUrl(url);// Insertion de l'URL de notre demande de connexion
+        req.addResponseListener(new ActionListener<NetworkEvent>() {
+            @Override
+            public void actionPerformed(NetworkEvent evt) {
+                resultOK = req.getResponseCode() == 200; //Code HTTP 200 OK
+                req.removeResponseListener(this); //Supprimer cet actionListener
+            }
+        });
+        NetworkManager.getInstance().addToQueueAndWait(req);
+        return resultOK;    }
+
+   
+}
---
+++ Mobile/ENERGYM/src/com/mycompany/services/ServiceUser.java
@@ -0,0 +1,142 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.services;
+
+import com.codename1.io.CharArrayReader;
+import com.codename1.io.ConnectionRequest;
+import com.codename1.io.JSONParser;
+import com.codename1.io.MultipartRequest;
+import com.codename1.io.NetworkManager;
+import com.codename1.ui.Dialog;
+import com.codename1.ui.TextField;
+import com.codename1.ui.util.Resources;
+import com.mycompany.entities.SessionManager;
+import com.mycompany.entities.User;
+import com.mycompany.gui.NewsfeedForm;
+import com.mycompany.utils.Statics;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ *
+ * @author MSI
+ */
+public class ServiceUser {
+
+    public ArrayList<User> User;
+
+    public static ServiceUser instance = null;
+    public boolean resultOK;
+    private ConnectionRequest req;
+    String json;
+
+    private ServiceUser() {
+        req = new ConnectionRequest();
+    }
+
+    public static ServiceUser getInstance() {
+        if (instance == null) {
+            instance = new ServiceUser();
+        }
+        return instance;
+    }
+
+    public void signup(TextField nom, TextField prenom, TextField email, TextField password, TextField confirmPassword, TextField Phone, Resources res) {
+        String url = Statics.BASE_URL + "/signupMobile?email=" + email.getText().toString() + "&nom=" + nom.getText().toString() + "&prenom=" + prenom.getText().toString() + "&password=" + password.getText().toString() + "&phoneNumber=" + Phone.getText().toString();
+        req.setUrl(url);
+        System.out.println(url);
+        if (email.getText().equals(" ") && password.getText().equals(" ")) {
+            Dialog.show("erreur", "veuillez remplir les champs", "ok", null);
+        }
+        req.addResponseListener((e) -> {
+            byte[] data = (byte[]) e.getMetaData();
+            String responseData = new String(data);
+            System.out.println("data===>" + responseData);
+        });
+        NetworkManager.getInstance().addToQueueAndWait(req);
+
+    }
+
+    public void signin(TextField email, TextField password, Resources res) {
+        String url = Statics.BASE_URL + "/signinMobile?email=" + email.getText().toString() + "&password=" + password.getText().toString();
+        req.setUrl(url);
+        System.out.println(url);
+
+        req.addResponseListener((e) -> {
+            JSONParser j = new JSONParser();
+            String json = new String(req.getResponseData()) + "";
+            try {
+                if (json.equals("failed")) {
+                    Dialog.show("echec d'authentification", "username or password incorrect", "OK", null);
+
+                } else {
+                    System.out.println("data ==" + json);
+                    Map<String, Object> user = j.parseJSON(new CharArrayReader(json.toCharArray()));
+                    if (!user.isEmpty()) {
+                        new NewsfeedForm(res).show();// yemchi lel home yelzem nrigelha
+                        SessionManager.setEmail(user.get("email").toString());
+                        float id = Float.parseFloat(user.get("id").toString());
+                        SessionManager.setId((int) id);
+                        //      SessionManager.setId();// hedhi mochkla
+                        SessionManager.setNom(user.get("nom").toString());
+                        SessionManager.setPrenom(user.get("prenom").toString());
+                        SessionManager.setPassword(user.get("password").toString());
+                    }
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
+
+        NetworkManager.getInstance().addToQueueAndWait(req);
+
+    }
+
+    public String getPasswordbyPhone(String phoneNumber, Resources res) {
+        String url = Statics.BASE_URL + "/passwordMobile?phoneNumber=" + phoneNumber;
+        req.setUrl(url);
+        System.out.println(url);
+
+        req.addResponseListener((e) -> {
+            JSONParser j = new JSONParser();
+            json = new String(req.getResponseData()) + "";
+            try {
+
+                System.out.println("data ==" + json);
+                Map<String, Object> password = j.parseJSON(new CharArrayReader(json.toCharArray()));
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
+
+        NetworkManager.getInstance().addToQueueAndWait(req);
+        return json;
+    }
+
+    public static void EditUser(String nom, String prenom, String email, String password,String imageFile) {
+        String url = Statics.BASE_URL + "/editUserMobile?&email=" + email + "&password=" + password + "&nom=" + nom + "&prenom=" + prenom+"&imageFile="+imageFile;
+        MultipartRequest req = new MultipartRequest();
+        req.setUrl(url);
+        req.setPost(true);
+        req.addArgument("nom", nom);
+        req.addArgument("prenom", prenom);
+        req.addArgument("password", password);
+        req.addArgument("email", email);
+        req.addResponseListener((response) -> {
+            byte[] data = (byte[]) response.getMetaData();
+            String a = new String(data);
+            System.out.println(a);
+            if (a.equals("success")) {
+            } else {
+                // Dialog.show("erreur", "echec de modification", "OK", null);
+            }
+        });
+        NetworkManager.getInstance().addToQueueAndWait(req);
+
+    }
+
+}
